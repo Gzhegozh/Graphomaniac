3c8493093fdeec849deffa7000eaa8ae0c0ea5cb
Gzhegozh <paulivanov95@gmail.com>
2016-02-18 16:21:06 +0300

    Fixed bug with updating chapter in db

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -6,15 +6,6 @@ class Chapter < ActiveRecord::Base
   end
 
   def self.update_by_order(params)
-    # connection = ActiveRecord::Base.connection
-    # connection.execute("UPDATE chapters SET
-    #       title = #{params[:title]},
-    #       content = #{params[:content]},
-    #       record_id = #{params[:record_id]}
-    #       WHERE \"order\" = #{params[:order]}");
-    # chapter = Chapter.find_by_order(params[:chapter][:order])
-    # byebug
-    # сhapter.update_attributes(title: params[:title], content: params[:content], record_id: params[:record_id])
-    Chapter.where(order: params[:chapter][:order]).limit(1).update_all(title: params[:chapter][:title], content: params[:chapter][:content], record_id: params[:chapter][:record_id])
+    Chapter.where(order: params[:chapter][:order], record_id: params[:chapter][:record_id]).limit(1).update_all(title: params[:chapter][:title], content: params[:chapter][:content])
   end
 end
